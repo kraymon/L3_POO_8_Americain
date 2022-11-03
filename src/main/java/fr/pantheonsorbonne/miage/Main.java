1251960377a0d1d9958b31927264db11399487e7
Abdel Malik Djendli <a.malik.djendli@gmail.com>
2022-11-03 15:39:48 +0100

    premier push

---
+++ src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -0,0 +1,5 @@
+package fr.pantheonsorbonne.miage;
+
+public class Main {
+    
+}
---
+++ src/main/java/fr/pantheonsorbonne/miage/Player.java
@@ -0,0 +1,17 @@
+package fr.pantheonsorbonne.miage;
+
+public class Player {
+    private String name;
+    private Hand hand = new Hand();
+
+    public Player (String name){
+        this.name = name;
+    }
+
+    public void pickCard(int number){
+    }
+
+    
+     
+
+}
