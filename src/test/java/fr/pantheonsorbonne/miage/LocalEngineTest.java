ae94cee8056a3b8e55b8bb26b5e960e47b8e1c3f
kraymon <raymond.ken11@gmail.com>
2022-12-05 23:21:00 +0100

    malik tests

--- src/test/java/fr/pantheonsorbonne/miage/LocalEngineTest.java
+++ src/test/java/fr/pantheonsorbonne/miage/LocalEngineTest.java
@@ -11,13 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class LocalEngineTest {
-
+    
     @Test
     public void testPlay() {
         Local local = new Local();
         local.play();
         assertEquals(0, local.getInitialPlayers()[local.getNextPlayer()].getHand().size());
     }
+    
     
     @Test
     public void testChangeRotation() {
@@ -118,5 +119,125 @@ public class LocalEngineTest {
         Distribution dist = new Distribution();
         Player p1 = new Player("test", dist.newRandomHand());
         assertEquals("test", p1.getName());
+    }
+
+    public void testAsPlayed(){
+        Local local = new Local();
+        int nbAsInHand = 0;
+        for(Card carte : local.getInitialPlayers()[0].getHand()){
+            if(carte.getValeur().equals("AS")){
+                nbAsInHand++;
+            }
+        }
+        local.asPlayed(local.getInitialPlayers()[0]);
+        assertEquals(nbAsInHand,local.getNbAs());
+        if(nbAsInHand==0){
+            assertTrue(local.getasPicked());
+        }
+        else{
+            assertFalse(local.getasPicked());
+        }
+    }
+    @Test
+    /*
+     * test si le huit est bien remove de la main
+     * test si le huit est bien add aux cartes jouées
+     */
+    public void testPlayEight(){
+        Local local = new Local();
+        int countEight = 0;
+        int indexHandWithEight =0;
+        for(int i=0; i<local.getInitialPlayers().length;i++){
+            for(Card card : local.getInitialPlayers()[i].getHand()){
+                if(card.getValeur().equals("HUIT")){
+                    countEight++;
+                    indexHandWithEight=i;
+                }
+            }
+        }
+        List<Card> test = new ArrayList<>();
+        Player player = new Player("test", test);
+        if(countEight<4){
+            Card carte = local.dist.getRandomCard();
+            String valeur=carte.getValeur();
+            while(valeur!="HUIT"){
+                carte = local.dist.getRandomCard();
+                valeur=carte.getValeur();
+            }
+            local.getInitialPlayers()[0].getHand().add(carte);
+            player = local.getInitialPlayers()[0];
+        }
+        else{
+            player = local.getInitialPlayers()[indexHandWithEight-1];
+        }
+        int indexEight = 0;
+        for(int i =0;i<player.getHand().size();i++){
+            if(player.getHand().get(i).getValeur().equals("HUIT")){
+                indexEight = i;
+            }
+        }
+        Card carte = player.getHand().get(indexEight);
+        local.playEight(indexEight, player);
+        assertTrue(local.dist.getPlayedCard().contains(carte));
+        assertFalse(local.getInitialPlayers()[0].getHand().contains(carte));
+    }
+    @Test
+    public void testPlayCombination(){
+        Local local = new Local();
+        int countValet = 0;
+        int indexHandWithValet =0;
+        for(Player player : local.getInitialPlayers()){
+            indexHandWithValet++;
+            for(Card card : player.getHand()){
+                if(card.getValeur().equals("VALET")){
+                    countValet++;
+                }
+            }
+        }
+        List<Card> test = new ArrayList<>();
+        Player player = new Player("test", test);
+        if(countValet<4){
+            Card carte = local.dist.getRandomCard();
+            String valeur=carte.getValeur();
+            while(valeur!="VALET"){
+                carte = local.dist.getRandomCard();
+                valeur=carte.getValeur();
+            }
+            local.getInitialPlayers()[0].getHand().add(carte);
+            player = local.getInitialPlayers()[0];
+        }
+        else{
+            player = local.getInitialPlayers()[indexHandWithValet-1];
+        }
+        countValet = 0;
+        for(Card card : player.getHand()){
+            if(card.getValeur().equals("VALET")){
+                countValet++;
+            }
+        }
+        local.playCombination("VALET",player);
+       boolean isValetDeleteFromHand = true;
+       for(Card card : player.getHand()){
+        if(card.getValeur().equals("VALET")){
+            isValetDeleteFromHand=false;
+            break;
+        }
+        assertTrue(isValetDeleteFromHand);
+        //assertEquals(countValet,local.dist.getPlayedCard().size());
+       }
+    }
+    @Test
+    /*
+     * si  10 => playagain true
+     * si Valet playagain et nombre de joueur >2 => changeRotation
+     * manque le test avec une partie de 2 joueur et
+     */
+    public void testverifyIfTenOrJack(){
+        Local local = new Local();
+        local.verifyIfTenOrJack("DIX");
+        assertTrue(local.getPlayAgain());
+        local.verifyIfTenOrJack("VALET");
+        assertFalse(local.getClockwise());
+        local.setPlayAgain();
     }
 }
